9b5469c069d79aa750ab246688d487d5acf6b1de
Finbar Schenk <164015408+DeadDevilPriest@users.noreply.github.com>
2025-10-09 13:53:18 +0200

    Update Ash_Hollow.java
    Updated register

--- Ash Hollow Requiem of the Dead/src/main/java/Ash_Hollow_Requiem/Ash_Hollow.java
+++ Ash Hollow Requiem of the Dead/src/main/java/Ash_Hollow_Requiem/Ash_Hollow.java
@@ -1,6 +1,5 @@
 package Ash_Hollow_Requiem;
 
-import Ash_Hollow_Requiem.effects.ModEffects;
 import software.bernie.geckolib.GeckoLib;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,13 +21,13 @@ public class Ash_Hollow {
         Ash_Hollow_Requiem.common.entities.ModEntities.ENTITIES.register(modEventBus);
         ModItems.CREATIVE_MODE_TABS.register(modEventBus);
         ModEffects.EFFECTS.register(modEventBus);
-        //ModDamageSources.DAMAGE.register(modEventBus);
-        //ModDamageTypes.DAMAGE.register(modEventBus);
 
         // Forge event listeners
         MinecraftForge.EVENT_BUS.register(AudioEventHandler.class);
         MinecraftForge.EVENT_BUS.register(KeyBindingScreen.class);
         MinecraftForge.EVENT_BUS.register(MicrophoneSelectionScreen.class);
         MinecraftForge.EVENT_BUS.register(AshHollowOptionScreen.class);
+        MinecraftForge.EVENT_BUS.register(ModDamageSources.class);
+        MinecraftForge.EVENT_BUS.register(ModEffects.class);
     }
 }
